0c1eeb42cb03de439e86a5030ea253b087d3b1c4
Torppo <pii.sainio@cs.helsinki.fi>
2013-10-21 20:36:10 +0300

    Sankari ja healthBar

---
+++ Hero/Assets/HeroMain.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HeroMain : MonoBehaviour {
+	public int maxHealth = 100;
+	public int health = 50;
+	
+	public float getHealth(){
+		return (float) health / maxHealth;
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
