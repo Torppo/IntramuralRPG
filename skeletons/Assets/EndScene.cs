891fe636883f76985e437b1f2a27dabf5cd01bdb
DocV <alphansuz@hotmail.com>
2013-11-06 14:10:52 +0200

    valmis(?)

---
+++ skeletons/Assets/EndScene.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndScene : MonoBehaviour {
+	
+	public GUIText wintext;
+	public float restartCountdown = 3f;
+	private bool endTriggered = false;
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Player"){
+			wintext.gameObject.SetActive(true);
+			GameObject.FindGameObjectWithTag("ScreenFader").GetComponent<Fader>().fadeToBlack = true;
+			endTriggered = true;
+		}
+	}
+	
+	void Update(){
+		if (endTriggered){
+			
+			if (restartCountdown >= 0f){
+				restartCountdown -= Time.deltaTime;	
+			}
+			else {
+				Application.LoadLevel(Application.loadedLevel);	
+			}
+		}	
+	}
+}
